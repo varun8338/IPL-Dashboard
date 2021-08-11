eaa6e21b815f3ce7779da0752e7b0685bee9c224
Varun <varun8338@test.com>
2021-08-11 09:28:56 +0530

    spring batch processor

--- src/main/java/com/dashboard/ipldashboard/data/MatchDataProcessor.java
+++ src/main/java/com/dashboard/ipldashboard/data/MatchDataProcessor.java
@@ -4,7 +4,6 @@ package com.dashboard.ipldashboard.data;
 import org.springframework.batch.item.ItemProcessor;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import com.dashboard.ipldashboard.model.Match;
 
@@ -22,7 +21,30 @@ public class MatchDataProcessor implements ItemProcessor<MatchInput,Match> {
     match.setDate(LocalDate.parse(matchInput.getDate()));
     match.setPlayerOfMatch(matchInput.getPlayer_of_match());
     match.setVenue(matchInput.getVenue());
-    
+
+    //set Team1 and Team2 depending on the innings order
+    String firstInningsTeam, secondInningsTeam;
+    if("bat".equals(matchInput.getToss_decision())){
+        firstInningsTeam = matchInput.getToss_winner();
+        secondInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1()) 
+        ? matchInput.getTeam2() : matchInput.getTeam1();
+    }
+    else{
+        secondInningsTeam = matchInput.getToss_winner();
+        firstInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1()) 
+        ? matchInput.getTeam2() : matchInput.getTeam1();
+    }
+
+    match.setTeam1(firstInningsTeam);
+    match.setTeam2(secondInningsTeam);
+
+    match.setTossWinner(matchInput.getToss_winner());
+    match.setTossDecision(matchInput.getToss_decision());
+    match.setResult(matchInput.getResult());
+    match.setResultMargin(matchInput.getResult_margin());
+    match.setUmpire1(matchInput.getUmpire1());
+    match.setUmpire2(matchInput.getUmpire2());
+
     return match;
   }
 }
