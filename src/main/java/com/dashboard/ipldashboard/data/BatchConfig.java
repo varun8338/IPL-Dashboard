f324d7906da5a54192f925dc59cfaffba2bc8830
Varun <varun8338@test.com>
2021-08-12 08:14:23 +0530

    Made Match object as Entity and partailly completed spring batch injestor

---
+++ src/main/java/com/dashboard/ipldashboard/data/BatchConfig.java
@@ -0,0 +1,61 @@
+package com.dashboard.ipldashboard.data;
+
+import javax.sql.DataSource;
+
+import com.dashboard.ipldashboard.model.Match;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@EnableBatchProcessing
+
+public class BatchConfig {
+
+    private final String[] FIELD_NAMES=new String[] {
+        "id","city","date","player_of_match","venue","neutral_venue","team1","team2","toss_winner","toss_decision","winner","result","result_margin","eliminator","method","umpire1","umpire2"
+    };
+
+
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public FlatFileItemReader<MatchInput> reader() {
+        return new FlatFileItemReaderBuilder<MatchInput>().name("MatchItemReader")
+                .resource(new ClassPathResource("match-data.csv")).delimited()
+                .names(FIELD_NAMES)
+                .fieldSetMapper(new BeanWrapperFieldSetMapper<MatchInput>() {
+                    {
+                        setTargetType(MatchInput.class);
+                    }
+                }).build();
+    }
+
+    @Bean
+    public MatchDataProcessor processor() {
+        return new MatchDataProcessor();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Match> writer(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Match>()
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .sql("INSERT INTO match (first_name, last_name) VALUES (:firstName, :lastName)").dataSource(dataSource)
+                .build();
+    }
+
+}
