bb3c217cbb3d5f9f4bc84d4076d3371851da48c8
Varun <varun8338@test.com>
2021-08-12 12:45:48 +0530

    Completed Spring Batch

--- src/main/java/com/dashboard/ipldashboard/data/BatchConfig.java
+++ src/main/java/com/dashboard/ipldashboard/data/BatchConfig.java
@@ -4,9 +4,13 @@ import javax.sql.DataSource;
 
 import com.dashboard.ipldashboard.model.Match;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
@@ -22,10 +26,9 @@ import org.springframework.core.io.ClassPathResource;
 
 public class BatchConfig {
 
-    private final String[] FIELD_NAMES=new String[] {
-        "id","city","date","player_of_match","venue","neutral_venue","team1","team2","toss_winner","toss_decision","winner","result","result_margin","eliminator","method","umpire1","umpire2"
-    };
-
+    private static final String[] FIELD_NAMES = new String[] { "id", "city", "date", "player_of_match", "venue",
+            "neutral_venue", "team1", "team2", "toss_winner", "toss_decision", "winner", "result", "result_margin",
+            "eliminator", "method", "umpire1", "umpire2" };
 
     @Autowired
     public JobBuilderFactory jobBuilderFactory;
@@ -35,14 +38,16 @@ public class BatchConfig {
 
     @Bean
     public FlatFileItemReader<MatchInput> reader() {
-        return new FlatFileItemReaderBuilder<MatchInput>().name("MatchItemReader")
-                .resource(new ClassPathResource("match-data.csv")).delimited()
-                .names(FIELD_NAMES)
-                .fieldSetMapper(new BeanWrapperFieldSetMapper<MatchInput>() {
-                    {
-                        setTargetType(MatchInput.class);
-                    }
-                }).build();
+        return new FlatFileItemReaderBuilder<MatchInput>()
+        .name("MatchItemReader")
+        .resource(new ClassPathResource("match-data.csv"))
+        .delimited().names(FIELD_NAMES)
+        .fieldSetMapper(new BeanWrapperFieldSetMapper<MatchInput>() {
+            {
+                setTargetType(MatchInput.class);
+            }
+        })
+        .build();
     }
 
     @Bean
@@ -54,8 +59,32 @@ public class BatchConfig {
     public JdbcBatchItemWriter<Match> writer(DataSource dataSource) {
         return new JdbcBatchItemWriterBuilder<Match>()
                 .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
-                .sql("INSERT INTO match (first_name, last_name) VALUES (:firstName, :lastName)").dataSource(dataSource)
+                .sql("INSERT INTO match (id, city, date, player_of_match, venue, team1, team2, toss_winner, toss_decision, match_winner, result, result_margin, umpire1, umpire2)"
+                        + "VALUES (:id, :city, :date, :playerOfMatch, :venue, :team1, :team2, :tossWinner, :tossDecision, :matchWinner, :result, :resultMargin, :umpire1, :umpire2)")
+                .dataSource(dataSource)
                 .build();
+    }
+
+    @Bean
+    public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
+        return jobBuilderFactory
+        .get("importUserJob")
+        .incrementer(new RunIdIncrementer())
+        .listener(listener)
+        .flow(step1)
+        .end()
+        .build();
+    }
+
+    @Bean
+    public Step step1(JdbcBatchItemWriter<Match> writer) {
+        return stepBuilderFactory
+        .get("step1")
+        .<MatchInput, Match>chunk(10)
+        .reader(reader())
+        .processor(processor())
+        .writer(writer)
+        .build();
     }
 
 }
